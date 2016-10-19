eef5ba9ea3e852222fa75f85853f6710e7d5063b
Pradeepa-Ann22 <pradeepa.ann.2010@gmail.com>
2016-10-19 20:27:46 +0530

    first check-in
    first check-in

--- rbs-core/src/main/java/com/pr/rbs/rbs_core/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pr.rbs.rbs_core.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pr.rbs.rbs_core.models.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
-	Customer findByMobile(Long mobile);
-}
